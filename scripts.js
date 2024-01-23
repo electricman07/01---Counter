4cb5eedcc934709db6b4431a3551e26befa28bf8
gpopowic <gmpopowich@gmail.com>
2024-01-23 12:00:36 -0800

    added counter script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-let counter = document.querySelector(".counter-numbers");
+const counter = document.querySelector(".counter-numbers");
+let count = localStorage.getItem("storageCounter");
+
+if (count === null) {
+  count = 0;
+}
+
+count++;
+
+localStorage.setItem("storageCounter", count);
+
+counter.innerHTML = count.toString().padStart(5, 0);
